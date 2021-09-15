9f8972879cda42726907fb318f1fa81db6b49597
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-09-15 18:50:37 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -1,103 +1,138 @@
-from asyncio import gather, sleep
+ 
+# Copyright (C) 2021 MoeZilla
 
-from pyrogram import filters
-from pyrogram.types import Message
-from aiohttp import ClientSession
-from Python_ARQ import ARQ
+# This file is part of Cutiepii (Telegram Bot)
 
-from SaitamaRobot import pbot as app
-from SaitamaRobot.utils.pluginhelp import edit_or_reply as eor
-from SaitamaRobot.pyrogramee.errors import capture_err
-from SaitamaRobot.utils.filter_groups import chatbot_group
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
-ARQ_API_URL = "https://thearq.tech"
-ARQ_API_KEY = 'JGDTCX-KQPBSC-VVMNMZ-BDJPIM-ARQ'
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 
-aiohttpsession = ClientSession()
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+import json
+import os
+import html
+import requests
+import Cutiepii_Robot.modules.sql.kuki_sql as sql
+
+from time import sleep
+from telegram import ParseMode
+from Cutiepii_Robot import dispatcher, updater, SUPPORT_CHAT
+from Cutiepii_Robot.modules.log_channel import gloggable
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram.error import BadRequest, RetryAfter, Unauthorized
+from telegram.ext import CommandHandler, run_async, CallbackContext, MessageHandler, Filters
+from Cutiepii_Robot.modules.helper_funcs.filters import CustomFilters
+from Cutiepii_Robot.modules.helper_funcs.chat_status import user_admin
+from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
+
+@user_admin
+@gloggable
+def add_chat(update: Update, context: CallbackContext):
+    chat = update.effective_chat
+    msg = update.effective_message
+    user = update.effective_user
+    is_kuki = sql.is_kuki(chat.id)
+    if not is_kuki:
+        sql.set_kuki(chat.id)
+        msg.reply_text("Cutiepii AI successfully enabled for this chat!")
+        message = (
+            f"<b>{html.escape(chat.title)}:</b>\n"
+            f"AI_ENABLED\n"
+            f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+        )
+        return message
+    msg.reply_text("Cutiepii AI is already enabled for this chat!")
+    return ""
 
 
-nigga = app
+@user_admin
+@gloggable
+def rem_chat(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    chat = update.effective_chat
+    user = update.effective_user
+    is_kuki = sql.is_kuki(chat.id)
+    if not is_kuki:
+        msg.reply_text("Cutiepii AI isn't enabled here in the first place!")
+        return ""
+    sql.rem_kuki(chat.id)
+    msg.reply_text("Cutiepii AI disabled successfully!")
+    message = (
+        f"<b>{html.escape(chat.title)}:</b>\n"
+        f"AI_DISABLED\n"
+        f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+    )
+    return message
+ 
+def chatbot(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    if not update.effective_message.chat.type == "private":
+        is_kuki = sql.is_kuki(chat_id)
+        if not is_kuki:
+            return
+    message = update.message.text
+    kukiurl = requests.get('https://kuki.up.railway.app/Kuki/chatbot?message='+message)
+    Kuki = json.loads(kukiurl.text)
+    kuki = Kuki['reply']
+    update.message.reply_text(kuki)
 
-meow = nigga.get_me()
+def list_all_chats(update: Update, context: CallbackContext):
+    chats = sql.get_all_kuki_chats()
+    text = "<b>KUKI-Enabled Chats</b>\n"
+    for chat in chats:
+        try:
+            x = context.bot.get_chat(int(*chat))
+            name = x.title if x.title else x.first_name
+            text += f"• <code>{name}</code>\n"
+        except BadRequest:
+            sql.rem_kuki(*chat)
+        except Unauthorized:
+            sql.rem_kuki(*chat)
+        except RetryAfter as e:
+            sleep(e.retry_after)
+    update.effective_message.reply_text(text, parse_mode="HTML")
+   
 
-BOT_ID = meow.id
+__help__ = f"""
+Chatbot utilizes the Kuki API and allows Yumeko to talk and provides a more interactive group chat experience.
 
-__mod_name__ = "ChatBot"
-__help__ = """
-`/chatbot enable` *:* To Enable ChatBot
-
-`/chatbot disable` *:* To Disable ChatBot
-
+*Commands:* 
+*Admins only:*
+   • `addchat`*:* Enables Chatbot mode in the chat.
+   • `rmchat`*:* Disables Chatbot mode in the chat.
+   
+Reports bugs at @Yumeko_Support
+*Powered by Moezilla* (https://github/moezilla) from @Kukichatbot
 """
 
-active_chats_bot = []
+__mod_name__ = "ChatBot"
 
-async def chat_bot_toggle(db, message: Message):
-    status = message.text.split(None, 1)[1].lower()
-    chat_id = message.chat.id
-    if status == "enable":
-        if chat_id not in db:
-            db.append(chat_id)
-            text = "Chatbot Enabled!"
-            return await eor(message, text=text)
-        await eor(message, text="ChatBot Is Already Enabled.")
-    elif status == "disable":
-        if chat_id in db:
-            db.remove(chat_id)
-            return await eor(message, text="Chatbot Disabled!")
-        await eor(message, text="ChatBot Is Already Disabled.")
-    else:
-        await eor(
-            message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]"
-        )
+ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat, run_async=True)
+REMOVE_CHAT_HANDLER = CommandHandler("rmchat", rem_chat, run_async=True)
+CHATBOT_HANDLER = MessageHandler(
+    Filters.text & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!")
+                    & ~Filters.regex(r"^\/")), chatbot)
+LIST_ALL_CHATS_HANDLER = CommandHandler(
+    "allchats", list_all_chats, filters=CustomFilters.dev_filter, run_async=True)
 
+dispatcher.add_handler(ADD_CHAT_HANDLER)
+dispatcher.add_handler(REMOVE_CHAT_HANDLER)
+dispatcher.add_handler(LIST_ALL_CHATS_HANDLER)
+dispatcher.add_handler(CHATBOT_HANDLER)
 
-# Enabled | Disable Chatbot
+__handlers__ = [
+    ADD_CHAT_HANDLER,
+    REMOVE_CHAT_HANDLER,
+    LIST_ALL_CHATS_HANDLER,
+    CHATBOT_HANDLER,
+]
 
 
-@app.on_message(filters.command("chatbot") & ~filters.edited)
-@capture_err
-async def chatbot_status(_, message: Message):
-    if len(message.command) != 2:
-        return await eor(
-            message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]"
-        )
-    await chat_bot_toggle(active_chats_bot, message)
-
-
-async def lunaQuery(query: str, user_id: int):
-    luna = await arq.luna(query, user_id)
-    return luna.result
-
-
-async def type_and_send(message: Message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id if message.from_user else 0
-    query = message.text.strip()
-    await message._client.send_chat_action(chat_id, "typing")
-    response, _ = await gather(lunaQuery(query, user_id), sleep(3))
-    await message.reply_text(response)
-    await message._client.send_chat_action(chat_id, "cancel")
-
-
-@app.on_message(
-    filters.text
-    & filters.reply
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.forwarded,
-    group=chatbot_group,
-)
-@capture_err
-async def chatbot_talk(_, message: Message):
-    if message.chat.id not in active_chats_bot:
-        return
-    if not message.reply_to_message:
-        return
-    if not message.reply_to_message.from_user:
-        return
-    if message.reply_to_message.from_user.id != BOT_ID:
-        return
-    await type_and_send(message)
