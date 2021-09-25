7127144d9f3693bf545b24797f19c09afe99776b
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-09-25 06:01:07 +0530

    Update welcome.py

--- SaitamaRobot/modules/welcome.py
+++ SaitamaRobot/modules/welcome.py
@@ -75,11 +75,11 @@ VERIFIED_USER_WAITLIST = {}
 
 OWNER_JOIN = """Praise yourself! Devil himself have joined the chat!!"""
 
-DEVIL = """https://telegra.ph/file/2ca148f1e4295c8e72969.jpg"""
+DEVIL = """https://telegra.ph/file/b63352645470c1593490f.jpg"""
 
 DEV_WEL = """https://telegra.ph/file/f9e591f0900ca59db2c0a.jpg"""
 
-SUDO_WEL = """https://telegra.ph/file/f9e591f0900ca59db2c0a.jpg"""
+SUDO_WEL = """https://telegra.ph/file/2ca148f1e4295c8e72969.jpg"""
 
 # do not async
 def send(update, message, keyboard, backup_message):
